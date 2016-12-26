9e3a6ed5f5b0a0afd274803c218e4623de9d5039
palmer <palmer.lao@gmail.com>
2016-12-25 19:12:30 -0500

    fixed divide by 0 in normalization

--- cryptopals/src/Set1.hs
+++ cryptopals/src/Set1.hs
@@ -1,16 +1,21 @@
 module Set1 (
   hex2base64,
   fixedXor,
+  topDecodings,
+  score
   )
        where
 
 import Data.Bits
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C
-import Data.Char (isDigit)
+import Data.Char (chr, isDigit, toLower)
 import Data.Int (Int8)
+import Data.List (foldl')
 import Data.Map (fromList, (!))
+import GHC.Exts (sortWith)
 
+-- challenge 1: convert hex to base64
 bin2base :: Int -> String -> Integer -> C.ByteString
 bin2base bits syms =
   let upper = 2^bits-1
@@ -34,5 +39,41 @@ int2hex = bin2base 4 hexSyms
 hex2int = base2bin 4 hexSyms
 hex2base64 = int2base64 . hex2int
 
+-- challenge2: xor two equal-length hex strings
 fixedXor :: C.ByteString -> C.ByteString -> C.ByteString
 fixedXor x y = int2hex $ (hex2int x) `xor` (hex2int y)
+
+-- challenge3: decode a hex string that was xor'd with one character
+hex2c = (bin2base 8 $ fmap chr [0..255]) .  (base2bin 4 hexSyms)
+encoded = C.pack "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+intEncoded = hex2int encoded
+hexKeys = [ C.pack [x,y] | x <- hexSyms, y <- hexSyms ]
+
+decode :: C.ByteString -> C.ByteString -> C.ByteString
+decode key str =
+  let keyBuf = C.concat $ take ((C.length encoded) `div` (C.length key)) $ repeat key
+  in  fixedXor keyBuf str
+
+normalize :: [(Char, Double)] -> [(Char, Double)]
+normalize counts =
+  let norm = foldl' (+) 0.0 $ fmap snd counts
+  in  if norm > 0
+      then fmap (\(c,f) -> (c, f/norm)) counts
+      else counts
+
+englishFreqs = normalize $ ['a'..'z'] `zip` [8.167,1.492,2.782,4.253,12.702,2.228,2.015,6.094,6.966,0.153,0.772,4.025,2.406,6.749,7.507,1.929,0.095,5.987,6.327,9.056,2.758,0.978,2.360,0.150,1.974,0.07]
+
+dist f1 f2 = foldl' (+) 0.0 $ [abs $ s1-s2 | (c1,s1) <- f1, (c2,s2) <- f2, c1==c2]
+
+score :: C.ByteString -> Double
+score str =
+  let freq = normalize $ fmap (\c -> (c, fromIntegral $ C.count c $ C.map toLower str)) ['a'..'z']
+  in  dist freq englishFreqs
+
+allDecodings :: [C.ByteString]
+allDecodings = fmap (\k -> decode k encoded) hexKeys
+
+topDecodings :: Int -> [C.ByteString]
+topDecodings k =
+  let messages = fmap hex2c allDecodings
+  in  take k $ sortWith score messages
--- cryptopals/test/Set1/Tests.hs
+++ cryptopals/test/Set1/Tests.hs
@@ -11,13 +11,12 @@ import Set1 (hex2base64, fixedXor)
 
 challenge1 :: Assertion
 challenge1 = assertEqual "hex2base64 example"
-  (hex2base64 . C.pack $ "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
   (C.pack "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+  (hex2base64 . C.pack $ "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 
 challenge2 :: Assertion
 challenge2 = assertEqual "fixed xor example"
+  (C.pack "746865206b696420646f6e277420706c6179")
   (fixedXor
    (C.pack "1c0111001f010100061a024b53535009181c")
    (C.pack "686974207468652062756c6c277320657965"))
-  (C.pack "746865206b696420646f6e277420706c6179")
-  
